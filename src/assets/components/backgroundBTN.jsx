4b61e2748ec8bcb0fadf852924fe234b65227323
rick-silva <ricksant.1@hotmail.com>
2023-02-01 12:00:04 -0300

    add button reset background

--- src/assets/components/backgroundBTN.jsx
+++ src/assets/components/backgroundBTN.jsx
@@ -9,16 +9,22 @@
             console.log(`${msg}`)
         }
 
+        const resetBackground = ()=> {
+            document.getElementById("body").style.backgroundImage = `url("")`
+        }
+
         return(
-            <div className="w-80 h-20 flex justify-center items-center bg-opacity-70 bg-white bg-clip-padding rounded-lg absolute top-10" >
+            
+                <div className="w-96 h-20 flex justify-center items-center bg-opacity-70 bg-white bg-clip-padding rounded-lg absolute top-10" >
 
-                <img className="w-24 h-12 cursor-pointer" onClick={() => {backgroundIMG("https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif", "bg1")}} src="https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif" alt=""/>
+                    <img className="w-24 h-12 cursor-pointer" onClick={() => {backgroundIMG("https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif", "bg1")}} src="https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif" alt=""/>
 
-                <img className="w-24 h-12 cursor-pointer" onClick={() => {backgroundIMG("https://www.pixelstalk.net/wp-content/uploads/images6/Lofi-Wallpapers-HD-Free-download.jpg", "bg2")}} src="https://www.pixelstalk.net/wp-content/uploads/images6/Lofi-Wallpapers-HD-Free-download.jpg" alt=""/>
+                    <img className="w-24 h-12 cursor-pointer" onClick={() => {backgroundIMG("https://www.pixelstalk.net/wp-content/uploads/images6/Lofi-Wallpapers-HD-Free-download.jpg", "bg2")}} src="https://www.pixelstalk.net/wp-content/uploads/images6/Lofi-Wallpapers-HD-Free-download.jpg" alt=""/>
 
-                <img className="w-24 h-12 cursor-pointer" onClick={() => {backgroundIMG("https://i.pinimg.com/originals/83/f6/5e/83f65e8c6efc88fabfcfbb11cf63bd8a.gif", "bg3")}} src="https://i.pinimg.com/originals/83/f6/5e/83f65e8c6efc88fabfcfbb11cf63bd8a.gif" alt=""/>
-
-            </div>
+                    <img className="w-24 h-12 cursor-pointer" onClick={() => {backgroundIMG("https://i.pinimg.com/originals/83/f6/5e/83f65e8c6efc88fabfcfbb11cf63bd8a.gif", "bg3")}} src="https://i.pinimg.com/originals/83/f6/5e/83f65e8c6efc88fabfcfbb11cf63bd8a.gif" alt=""/>
+                <button className="w-16 m-2.5 rounded-md bg-gradient-to-r from-green-400 to-blue-500" onClick={resetBackground}>Reset</button>
+                </div>
+            
         )
         
 }//fim component
