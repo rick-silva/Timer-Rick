d24ffe1850ea04ddaa97d351a41790b7049d0a5e
rick-silva <ricksant.1@hotmail.com>
2023-02-24 14:28:23 -0300

    add wallpaper mobile

--- src/assets/components/backgroundBTN.jsx
+++ src/assets/components/backgroundBTN.jsx
@@ -13,20 +13,34 @@
             document.getElementById("body").style.backgroundImage = `url("")`
         }
 
-        return(
-            
+        if(window.screen.width <= 500){
+            return(
                 <div className="w-96 h-20 flex justify-center items-center bg-opacity-70 bg-white bg-clip-padding rounded-lg absolute top-10 max-lg:w-80 max-lg:h-16 max-md:w-72 max-md:h-14" >
 
-                    <img className="w-24 h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif", "bg1")}} src="https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif" alt=""/>
-
-                    <img className="w-24  h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://www.pixelstalk.net/wp-content/uploads/images6/Lofi-Wallpapers-HD-Free-download.jpg", "bg2")}} src="https://www.pixelstalk.net/wp-content/uploads/images6/Lofi-Wallpapers-HD-Free-download.jpg" alt=""/>
-
-                    <img className="w-24 h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://i.pinimg.com/originals/83/f6/5e/83f65e8c6efc88fabfcfbb11cf63bd8a.gif", "bg3")}} src="https://i.pinimg.com/originals/83/f6/5e/83f65e8c6efc88fabfcfbb11cf63bd8a.gif" alt=""/>
+                    <img className="w-24 h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://media.giphy.com/media/Eqz8ZFUScPHH2/giphy.gif", "bg1")}} src="https://media.giphy.com/media/Eqz8ZFUScPHH2/giphy.gif" alt=""/>
                     
+                    <img className="w-24  h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://i.pinimg.com/originals/27/86/26/278626e6a8d3e055fa4ea6ccb53aff8d.png", "bg2")}} src="https://i.pinimg.com/originals/27/86/26/278626e6a8d3e055fa4ea6ccb53aff8d.png" alt=""/>
+
+                    <img className="w-24 h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://mobimg.b-cdn.net/v3/fetch/62/62e3ce60fc426fe6f475764cd99779b9.jpeg", "bg3")}} src="https://mobimg.b-cdn.net/v3/fetch/62/62e3ce60fc426fe6f475764cd99779b9.jpeg" alt=""/>
+
                     <button className="w-16 m-2.5 rounded-md bg-gradient-to-r from-green-400 to-blue-500 max-lg:w-14 max-lg:m-2 max-md:m-1.5" onClick={resetBackground}>Reset</button>
                 </div>
-            
-        )
+               
+            )
+        }else{
+            return(
+                    <div className="w-96 h-20 flex justify-center items-center bg-opacity-70 bg-white bg-clip-padding rounded-lg absolute top-10 max-lg:w-80 max-lg:h-16 max-md:w-72 max-md:h-14" >
+
+                        <img className="w-24 h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif", "bg1")}} src="https://user-images.githubusercontent.com/36126914/154766367-2c3d9c80-3cdc-4790-b15c-7eba5eee9fd2.gif" alt=""/>
+
+                        <img className="w-24  h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://www.pixelstalk.net/wp-content/uploads/images6/Lofi-Wallpapers-HD-Free-download.jpg", "bg2")}} src="https://www.pixelstalk.net/wp-content/uploads/images6/Lofi-Wallpapers-HD-Free-download.jpg" alt=""/>
+
+                        <img className="w-24 h-12 cursor-pointer max-lg:w-20 max-lg:h-11 max-md:w-16 max-md:h-10" onClick={() => {backgroundIMG("https://i.pinimg.com/originals/83/f6/5e/83f65e8c6efc88fabfcfbb11cf63bd8a.gif", "bg3")}} src="https://i.pinimg.com/originals/83/f6/5e/83f65e8c6efc88fabfcfbb11cf63bd8a.gif" alt=""/>
+                        
+                        <button className="w-16 m-2.5 rounded-md bg-gradient-to-r from-green-400 to-blue-500 max-lg:w-14 max-lg:m-2 max-md:m-1.5" onClick={resetBackground}>Reset</button>
+                    </div>     
+            )
+        }
         
 }//fim component
 
