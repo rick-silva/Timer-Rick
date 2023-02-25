8bb70a7549f6e33d9b8cafcb3ea5edbf305b5836
rick-silva <ricksant.1@hotmail.com>
2023-02-25 10:42:55 -0300

    test song

--- src/assets/components/player.jsx
+++ src/assets/components/player.jsx
@@ -10,12 +10,7 @@ export const Player = () =>{
 
     return(
         <div className="w-96 absolute bottom-10">
-            {/* <iframe width="260" height="115" 
-                src="https://www.youtube-nocookie.com/embed/BEXL80LS0-I?controls=0&autoplay=1&loop=1" 
-                title="YouTube video player"
-                allowfullscreen>
-
-            </iframe> */}
+            <audio controls src="src/assets/music/GTA5_Message.mp3"></audio>
         </div>
     )
 }
